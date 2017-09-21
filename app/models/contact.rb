06331f1744ff4117d4c3764d2de34c6123f022e2
wobblefish <matt.mcneil1984@gmail.com>
2017-09-21 21:27:44 +0000

    Added contact_form validation

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,5 @@
 class Contact < ActiveRecord::Base
-    #name
-    #email
-    #comments
+    validates :name, presence: true
+    validates :email, presence: true
+    validates :comments, presence: true
 end
