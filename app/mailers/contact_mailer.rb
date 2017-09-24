98caa120dd9ee24b17e950ecbc1e8b1cd13e76c9
wobblefish <matt.mcneil1984@gmail.com>
2017-09-24 23:02:22 +0000

    Fixed issue with contacts mailer

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,10 +1,11 @@
 class ContactMailer < ActionMailer::Base
    default to: 'matt.mcneill1984@outlook.com'
    
-   def contact_email
+   def contact_email(name, email, body)
      @name = name
      @email = email
      @body = body
      
      mail(from: email, subject: 'Contact Form Message')
+   end
 end
