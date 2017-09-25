976e1cb0143a8656860ddcfbdfba59428bc76ee7
wobblefish <matt.mcneil1984@gmail.com>
2017-09-25 00:49:49 +0000

    Tweaked mailer settings

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ActionMailer::Base
-   default to: 'matt.mcneill1984@outlook.com'
+   default to: 'matt.mcneil1984@gmail.com'
    
    def contact_email(name, email, body)
      @name = name
