1857041a95fb6a42b4fd869d80f20f1f75366a00
wobblefish <matt.mcneil1984@gmail.com>
2017-10-04 02:11:05 +0000

    Added profiles create action / fixed new action to correct form

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,5 +2,27 @@ class ProfilesController < ApplicationController
   # GET request to users/:user_id/profile/new
   def new
     # Render blank profile details form
+    @profile = Profile.new
   end
+  
+  # POST request to /users/:user_id/profile
+  def create
+    # Ensure that we have the user who is filling out form
+    @user = User.find( params[:user_id] )
+    # Create profile linked to this specific user
+    @profile = @user.build_profile ( profile_params )
+    if @profile.save
+      flash[:success] = "Profile is updated!"
+      redirect_to root_path
+    else
+      render action: :new
+    end
+  end
+  
+  # Whitelisted profile fields
+  private
+    def profile_params
+      params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
+    end
+  
 end
