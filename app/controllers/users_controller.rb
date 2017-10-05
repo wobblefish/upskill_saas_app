7147c1d53981ab96759fba9fa1388bb51dcf3faf
wobblefish <matt.mcneil1984@gmail.com>
2017-10-05 05:02:36 +0000

    Added users  index action and view for Community section

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-
+  
+  # GET to /users
+  def index
+    
+  end
   #GET to users/id
   def show
     @user = User.find( params[:id] )
